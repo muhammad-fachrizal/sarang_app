cdf640ec845bf98b61ed9452130a55c20d190333
Muhammad Fachrizal <mfachrizal17@gmail.com>
2023-04-20 17:48:02 +0700

    Create Image Picker Util

--- lib/src/features/authentication/presentation/sign_up_upload_photo_screen.dart
+++ lib/src/features/authentication/presentation/sign_up_upload_photo_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:sarang_app/src/common_widgets/custom_button_widget.dart';
 import 'package:sarang_app/src/common_widgets/custom_text_button_widget.dart';
@@ -7,6 +9,7 @@ import 'package:sarang_app/src/features/likes_you/presentation/explore_people_sc
 import 'package:sarang_app/src/theme_manager/font_manager.dart';
 import 'package:sarang_app/src/theme_manager/style_manager.dart';
 import 'package:sarang_app/src/theme_manager/values_manager.dart';
+import 'package:sarang_app/src/utils/image_picker_util.dart';
 
 class SignUpUploadPhotoScreen extends StatefulWidget {
   static const String routeName = '/sign-up-upload-photo';
@@ -18,6 +21,16 @@ class SignUpUploadPhotoScreen extends StatefulWidget {
 }
 
 class _SignUpUploadPhotoScreenState extends State<SignUpUploadPhotoScreen> {
+  File? image;
+  void getImageProfile(GetImageFrom source) async {
+    final result = await ImagePickerUtil.getImage(source);
+    if (result != null) {
+      setState(() {
+        image = File(result.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +46,46 @@ class _SignUpUploadPhotoScreenState extends State<SignUpUploadPhotoScreen> {
                 const SizedBox(
                   height: AppSize.s50,
                 ),
-                const UploadPhotoWidget(),
+                GestureDetector(
+                  onTap: () {
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return Container(
+                          padding: const EdgeInsets.all(
+                            AppPadding.p24,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  getImageProfile(GetImageFrom.camera);
+                                },
+                                icon: const Icon(
+                                  Icons.camera,
+                                  size: AppSize.s50,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  getImageProfile(GetImageFrom.galerry);
+                                },
+                                icon: const Icon(
+                                  Icons.photo,
+                                  size: AppSize.s50,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  child: UploadPhotoWidget(
+                    image: image,
+                  ),
+                ),
                 const SizedBox(
                   height: 53.0,
                 ),
